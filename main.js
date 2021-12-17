0b17a4dc1652c11a0a7e196fc56ace646f66d8d1
ShivankParashar <83078938+ShivankParashar@users.noreply.github.com>
2021-12-17 18:38:42 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+var canvas = new fabric.Canvas('myCanvas');
+ var x= document.getElementById("myAudio");
+
+function new_image()
+{
+	fabric.Image.fromURL('BirthdayImage.jpg', function(Img) {
+        block_img_obj = Img;
+        block_img_obj.scaleToWidth(700);
+        block_img_obj.scaleToHeight(500);
+        block_img_obj.set({
+            top:0,
+            left:0
+        });
+        canvas.add(block_img_obj);
+    });
+}
+
+function playS(){
+	x.play();
+}
